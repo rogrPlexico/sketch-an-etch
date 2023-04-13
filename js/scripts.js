149cc25a6b4cef8b504d6b7ebb1b3a49d3f6b64e
rogrPlexico <96746687+rogrPlexico@users.noreply.github.com>
2023-04-13 16:06:29 -0400

    add colorful draw feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
-function active(e) {
-    const element = e.target;
-    element.classList.add('active');
-}
 
+// gives user option to change grid zize when button is clicked
 function buttonClick() {
-    
     do {
         var numberOfSquares = prompt('Enter Number of Squares Per Side', 'enter a number 2 - 100');
         console.log(numberOfSquares);
     } while (!(numberOfSquares > 0 && numberOfSquares < 151)
         && numberOfSquares !== null);
-
     
     // remove existing grid
     while (gridContainer.firstChild) {
@@ -32,14 +27,29 @@ function buttonClick() {
     }
 }
 
+
+function active(e) {
+    const cell = e.target;
+    cell.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+
+
+}
+
 const gridContainer = document.querySelector('.grid-container') 
 
-for (i = 1; i <= 16; i++) {
+for (i = 1; i <= 3; i++) {
     const row = document.createElement('div');
     row.classList.add(`row${i}`, 'row');
     gridContainer.appendChild(row);
 
-    for (j =1; j <= 16; j++) {
+    for (j =1; j <= 3; j++) {
         const column = document.createElement('div');
         column.classList.add(`column${j}`, 'column');
         column.addEventListener('mouseenter', active)
@@ -51,4 +61,10 @@ for (i = 1; i <= 16; i++) {
 
 const button = document.querySelector('button');
 button.addEventListener('click', buttonClick);
+
+
+
+
+
+
 
